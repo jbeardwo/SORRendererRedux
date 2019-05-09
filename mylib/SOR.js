7d37229d8869cf034e6f8fe7c86f6e4eb6be571d
jbeardwo <jfwbeardwood@gmail.com>
2019-05-08 20:24:58 -0700

    Calculation of normals for smooth shading completed
    next I will add a draw normals feature to check if they're correct

--- mylib/SOR.js
+++ mylib/SOR.js
@@ -5,22 +5,25 @@ function coord(x, y, z) {
 }
 
 function mySORClass(name, baseLine, color) {
-    this.name = name
-    this.baseLine = baseLine
-    this.shape = this.generateSOR()
-    this.color = color
+    this.name = name;
+    this.baseLine = baseLine;
+    this.shape = this.generateSOR();
+    this.color = color;
 
-    this.vertices = this.calcVertices()
-    this.indices = this.calcIndices()
+    this.vertices = this.calcVertices();
+    this.indices = this.calcIndices();
+
+    this.faceNormals = this.calcFaceNormals();
+    this.smoothNormals = this.calcSmoothNormals();
 }
 
 mySORClass.prototype.generateSOR = function() {
-    var x
-    var y
-    var z
-    var radians
-    var currentLine
-    var shape = [] //a completed shape is 36 lines, each are their own array
+    var x;
+    var y;
+    var z;
+    var radians;
+    var currentLine;
+    var shape = []; //a completed shape is 36 lines, each are their own array
 
     for (var angle = 0; angle <= 360; angle += 10) {
         radians = ((angle * Math.PI) / 180)
@@ -39,8 +42,8 @@ mySORClass.prototype.generateSOR = function() {
 
 mySORClass.prototype.calcVertices = function() {
     var vertices = []
-    for(i = 0; i < this.shape.length; i++) {
-        for(j = 0; j < this.shape[0].length; j++){
+    for(var i = 0; i < this.shape.length; i++) {
+        for(var j = 0; j < this.shape[0].length; j++){
             vertices.push(this.shape[i][j].x)
             vertices.push(this.shape[i][j].y)
             vertices.push(this.shape[i][j].z)
@@ -53,7 +56,7 @@ mySORClass.prototype.calcIndices = function() {
     var indices = []
     var lineSize = this.shape[0].length
     for (var i = 0; i < this.shape.length - 1; i++) {
-        for (j = 0; j < lineSize - 1 ; j++) {
+        for (var j = 0; j < lineSize - 1 ; j++) {
             indices.push(i*lineSize+j,i*lineSize+j+1,(i+1)*lineSize+j+1);
             indices.push(i*lineSize+j,(i+1)*lineSize+j+1,(i+1)*lineSize+j);
         }
@@ -61,6 +64,128 @@ mySORClass.prototype.calcIndices = function() {
     return indices;
 }
 
+mySORClass.prototype.calcFaceNormals = function() {
+    faceNormals = []
+    for (var i = 0; i < this.shape.length - 1; i++) {
+        for (var j = 0; j < this.shape[0].length - 1; j++) {
+            var currentLine = this.shape[i]
+            var nextLine = this.shape[i + 1]
+            faceNormals.push(calculateNormal(currentLine[j], currentLine[j + 1], nextLine[j]))
+        }
+    }
+    console.log(faceNormals)
+    return faceNormals
+}
+
+
+mySORClass.prototype.calcSmoothNormals = function() {
+    smoothNormals = [];
+    currentNormal = [];
+    addedNormal = [];
+    baseSize = this.baseLine.length/3;
+    //Vertex normal for smooth shading is calculating by adding Normals of all adjascent faces
+
+
+    //This handles wraparound for the first set of vertices
+    //This is the first vertex
+    //bottom Right
+    currentNormal = this.faceNormals[0];
+    //bottom Left (wraparound)
+    addedNormal = this.faceNormals[this.faceNormals.length-(baseSize-1)]
+    console.log(currentNormal)
+    console.log(addedNormal)
+    currentNormal = normalize(addVectors(currentNormal,addedNormal));
+    smoothNormals.push(currentNormal)
+    //This is 2nd through 2nd to last vertex in the first set of vertices
+    for(var i = 1;i<baseSize-1;i++){
+        //top right
+        currentNormal = this.faceNormals[i-1];
+        //bottom right
+        addedNormal = this.faceNormals[i];
+        currentNormal = normalize(addVectors(currentNormal, addedNormal));
+        //top left (wraparound)
+        addedNormal = this.faceNormals[this.faceNormals.length-(baseSize-i)];
+        currentNormal = normalize(addVectors(currentNormal, addedNormal));
+        //bottom left (wraparound)
+        addedNormal = this.faceNormals[this.faceNormals.length-(baseSize-i-1)];
+        currentNormal = normalize(addVectors(currentNormal, addedNormal));
+        smoothNormals.push(currentNormal);
+    }
+    //This is the last vertex from the first set
+    //top Right
+    currentNormal = this.faceNormals[baseSize-1];
+    //top Left (wraparound)
+    addedNormal = this.faceNormals[this.faceNormals.length-1];
+    currentNormal = normalize(addVectors(currentNormal, addedNormal));
+    smoothNormals.push(currentNormal);
+
+
+    //The middle of the set:
+    //top of each group
+    for(var i = 1;i < this.shape.length - 1; i++){
+        //bottom right
+        currentNormal = this.faceNormals[baseSize*i-i];
+        //bottom left
+        addedNormal = this.faceNormals[baseSize*i-(baseSize/3+(i-1))];
+        currentNormal = normalize(addVectors(currentNormal,addedNormal));
+        smoothNormals.push(currentNormal);
+        //middle of each group
+        for(var j = 1; j < baseSize-1; j++){
+            //top right
+            currentNormal = this.faceNormals[baseSize*i-i + (j-1)];
+            //bottom right
+            addedNormal = this.faceNormals[baseSize*i-i+j];
+            currentNormal = normalize(addVectors(currentNormal,addedNormal));
+            //top left
+            addedNormal = this.faceNormals[baseSize*i-(baseSize+(i-1)) + (j-1)];
+            currentNormal = normalize(addVectors(currentNormal,addedNormal));
+            //bottom left
+            addedNormal = this.faceNormals[baseSize*i-(baseSize+(i-1)) + j];
+            currentNormal = normalize(addVectors(currentNormal,addedNormal));
+            smoothNormals.push(currentNormal);
+        }
+        //bottom of each group
+        //top right
+        currentNormal = this.faceNormals[baseSize*i-i + (baseSize-2)];
+        //top left
+        addedNormal = this.faceNormals[baseSize*i-(baseSize+(i-1)) + (baseSize-2)];
+        currentNormal = normalize(addVectors(currentNormal,addedNormal));
+        smoothNormals.push(currentNormal);
+    }
+
+    //Final vertices are duplicates of first ones, so wrapping around from the other side:
+    //top of final group:
+    //bottom right
+    currentNormal = this.faceNormals[0];
+    //bottom left
+    addedNormal = this.faceNormals[this.faceNormals.length - 1 - (baseSize-2)];
+    currentNormal = normalize(addVectors(currentNormal,addedNormal));
+    smoothNormals.push(currentNormal);
+    //middle of final group
+    for(var i = 1; i < baseSize-1; i++){
+        //top right
+        currentNormal = this.faceNormals[i-1];
+        //bottom right
+        addedNormal = this.faceNormals[i];
+        currentNormal = normalize(addVectors(currentNormal,addedNormal));
+        //top left
+        addedNormal = this.faceNormals[this.faceNormals.length - 1 - (baseSize-2) + (i-1)];
+        currentNormal = normalize(addVectors(currentNormal,addedNormal));
+        //bottom left
+        addedNormal = this.faceNormals[this.faceNormals.length - 1 - (baseSize-2) + i];
+        currentNormal = normalize(addVectors(currentNormal,addedNormal));
+        smoothNormals.push(currentNormal);
+    }
+    //final vertex of final group
+    //top right
+    currentNormal = this.faceNormals[baseSize-2];
+    //top left
+    addedNormal = this.faceNormals[this.faceNormals.length-1];
+    currentNormal = normalize(addVectors(currentNormal,addedNormal));
+    smoothNormals.push(currentNormal);
+
+    return smoothNormals;
+}
 
 
 mySORClass.prototype.draw = function() {
@@ -88,7 +213,7 @@ mySORClass.prototype.draw = function() {
 
     gl.enable(gl.DEPTH_TEST)
     var mvpMatrix = new Matrix4()
-    mvpMatrix.setOrtho(-500, 500, -500, 500, -500, 500)
+    mvpMatrix.setOrtho(-500, 500, -500, 500, -5000, 5000)
 
     gl.uniformMatrix4fv(u_MvpMatrix, false, mvpMatrix.elements)
     gl.drawElements(gl.TRIANGLES, drawIndices.length, gl.UNSIGNED_SHORT, 0)
--- mylib/psuedoVectorOperations.js
+++ mylib/psuedoVectorOperations.js
@@ -25,3 +25,19 @@ function normalize(vector) {
     }
     return output
 }
+
+function calculateNormal(pt0, pt1, pt2) {
+    var cross0 = []
+    var cross1 = []
+    var crossProd = []
+
+    cross0.push(pt1.x - pt0.x, pt1.y - pt0.y, pt1.z - pt0.z)
+    cross1.push(pt2.x - pt0.x, pt2.y - pt0.y, pt2.z - pt0.z)
+
+    crossProd = [cross0[1] * cross1[2] - cross0[2] * cross1[1], cross0[2] * cross1[0] - cross0[0] * cross1[2], cross0[0] * cross1[1] - cross0[1] * cross1[0]]
+    var magnitude = Math.sqrt(Math.pow(crossProd[0], 2) + Math.pow(crossProd[1], 2) + Math.pow(crossProd[2], 2))
+    for (var k = 0; k < 3; k++) {
+        crossProd[k] = (crossProd[k] / magnitude)
+    }
+    return crossProd
+}
